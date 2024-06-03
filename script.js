44dd9559db41a87f64da592744263590902d23b0
ebnyame <ebnyame001@st.ug.edu.gh>
2024-06-03 13:40:06 -0700

    another refactoring

--- script.js
+++ script.js
@@ -6,30 +6,37 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 console.log(btnsOpenModal);
 
+// *******Refactoring the code********
+
+// for opening the modal
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+
+  // closing the modal
+};
+
+// for closing modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+  btnsOpenModal[i].addEventListener('click', openModal);
 
-    // Refactoring the code
-    const closeModal = function () {
-      modal.classList.add('hidden');
-      overlay.classList.add('hidden');
-    };
-    // closing the modal
-    btnCloseModal.addEventListener(
-      'click',
-      closeModal
-      //   modal.classList.add('hidden');
-      //   overlay.classList.add('hidden');
-    );
+btnCloseModal.addEventListener(
+  'click',
+  closeModal
+  //   modal.classList.add('hidden');
+  //   overlay.classList.add('hidden');
+);
 
-    // clicking empty space to close the modal
-    overlay.addEventListener(
-      'click',
-      closeModal
-      //   modal.classList.add('hidden');
-      //   overlay.classList.add('hidden');
-    );
-  });
+// clicking empty space to close the modal
+overlay.addEventListener(
+  'click',
+  closeModal
+  //   modal.classList.add('hidden');
+  //   overlay.classList.add('hidden');
+);
